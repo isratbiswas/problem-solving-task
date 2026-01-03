8add2e48b1e15965a974662b5fc3614124554bca
isratbiswas <isratbiswas28@gmail.com>
2026-01-03 19:29:45 +0600

    Task 7  completed

--- index.js
+++ index.js
@@ -87,3 +87,13 @@ const sumEven = (arr) => {
 };
 
 console.log(sumEven(numbers));
+
+//task -7
+
+const isLeapYear = (year) => {
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  return year % 4 === 0;
+};
+
+console.log(isLeapYear(2026));
